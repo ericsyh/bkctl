e87716985b8ee8a1e958a822c3825c82c26f360a
Eric Shen <ericshenyuhao@outlook.com>
2022-05-03 08:20:13 +0800

    add bookie list command
    Signed-off-by: Eric Shen <ericshenyuhao@outlook.com>

--- cmd/api.go
+++ cmd/api.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
+
+var bklist map[string]string
 
 type BookieInfo struct {
 	FreeSpace  int64 `json:"freeSpace"`
@@ -30,6 +33,16 @@ func diskfile(url string) []byte {
 	resp, err := http.Get("http://" + url + "/api/v1/bookie/list_disk_file")
 	if err != nil {
 		fmt.Println("Error:", err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	return body
+}
+
+func bookielist(url string) []byte {
+	resp, err := http.Get("http://" + url + "/api/v1/bookie/list_bookies")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
 	}
 	body, err := ioutil.ReadAll(resp.Body)
 	return body
